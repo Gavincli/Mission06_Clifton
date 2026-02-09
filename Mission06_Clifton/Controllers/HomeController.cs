9e84b5d52800ae505f3e9ed362efdea295b9f138
GavinClifton <166751534+Gavincli@users.noreply.github.com>
2026-02-09 15:13:15 -0700

    Add project files.

---
+++ Mission06_Clifton/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Clifton.Models;
+
+namespace Mission06_Clifton.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
