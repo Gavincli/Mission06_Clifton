eb036f5015e5512452712af2c37bfd0c22f720ba
GavinClifton <166751534+Gavincli@users.noreply.github.com>
2026-02-20 23:41:13 -0700

    Edit and Delete buttons, function and views.

--- Mission06_Clifton/Controllers/HomeController.cs
+++ Mission06_Clifton/Controllers/HomeController.cs
@@ -44,5 +44,55 @@ namespace Mission06_Clifton.Controllers
             var movies = _context.Movies.ToList(); // Fetch all movies
             return View(movies);                   // Pass to view
         }
+
+        // GET: show the form with existing data
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: submit the changes
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Update(movie);
+                _context.SaveChanges();
+                return RedirectToAction("MovieList");
+            }
+            return View(movie);
+        }
+
+        // GET: Confirm delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: actually delete
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("MovieList");
+        }
     }
 }
